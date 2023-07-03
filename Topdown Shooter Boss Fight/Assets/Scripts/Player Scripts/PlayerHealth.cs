d593f0d45e5cb6cb68588a14912b4606d32a1a83
Century178 <mrothenberger0918@gmail.com>
2023-07-03 16:05:22 -0700

    Invuln. frames
    For a short period at the start of the game and after getting hit, you will take no damage. This is shown by the player being half-transparent.

--- Topdown Shooter Boss Fight/Assets/Scripts/Player Scripts/PlayerHealth.cs
+++ Topdown Shooter Boss Fight/Assets/Scripts/Player Scripts/PlayerHealth.cs
@@ -7,27 +7,53 @@ public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private int health = 3;
 
+    private float invulnTime;
+    [SerializeField] private float invulnLimit;
+
     [SerializeField] private TextMeshProUGUI healthText;
+
+    private SpriteRenderer sr;
 
     private void Awake()
     {
+        sr = GetComponent<SpriteRenderer>();
         Time.timeScale = 1f;
         healthText.text = "HP: " + health.ToString();
+        invulnTime = invulnLimit;
+    }
+
+    private void Update()
+    {
+        if (invulnTime > 0)
+        {
+            invulnTime -= Time.deltaTime;
+            Physics2D.IgnoreLayerCollision(3, 6);
+            Physics2D.IgnoreLayerCollision(3, 7);
+            sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, 0.5f);
+        }
+        else
+        {
+            Physics2D.IgnoreLayerCollision(3, 6, false);
+            Physics2D.IgnoreLayerCollision(3, 7, false);
+            sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, 1f);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            health=0;
+            health = 0;
             healthText.text = "HP: " + health.ToString();
             Cursor.visible = true;
             Destroy(gameObject);
             Time.timeScale = 0f;
-        }else if (collision.gameObject.CompareTag("EnemyBullet"))
+        }
+        else if (collision.gameObject.CompareTag("EnemyBullet"))
         {
-            health --;
+            health--;
             healthText.text = "HP: " + health.ToString();
+            invulnTime = invulnLimit;
             if (health > 0)
             {
                 return;
